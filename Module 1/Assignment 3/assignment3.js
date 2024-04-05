39c61c8238f1ae0268b73bc6dfc7b3e75e3a4ca7
onnik <onni.kivinen@gmail.com>
2024-04-05 13:33:13 +0300

    Add Module 1 Assignment 3 files

---
+++ Module 1/Assignment 3/assignment3.js
@@ -0,0 +1,22 @@
+// Numero promptit.
+const number = parseInt(prompt(`1. Enter a number: `));
+const number2 = parseInt(prompt(`2. Enter another number: `));
+const number3 = parseInt(prompt(`3. Enter a third number: `));
+
+// Summa, tulo ja keskiarvo muunnokset.
+sum = number + number2 + number3;
+product = number * number2 * number3;
+average = sum / 3;
+
+// Viittaukset div-elementteihin, <div id="sum"></div> tyyppisesti, querySelectorissa pitää muistaa # merkki.
+const sumElement = document.querySelector("#sum");
+console.log(sumElement); // Debug consoleen
+sumElement.textContent = "The sum of your numbers is " + sum + ".";
+
+const productElement = document.querySelector("#product");
+console.log(productElement); // Debug consoleen
+productElement.textContent = "The product of your numbers is " + product + ".";
+
+const averageElement = document.querySelector("#average");
+console.log(averageElement); // Debug consoleen
+averageElement.textContent = "The average of your numbers is " + average + ".";
