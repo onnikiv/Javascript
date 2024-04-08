1dd7bf435548999b4637225bfcf90ada9517e91a
onnik <onni.kivinen@gmail.com>
2024-04-08 11:23:57 +0300

    finish

--- Module 1/Assignment 6/assignment6.js
+++ Module 1/Assignment 6/assignment6.js
@@ -2,17 +2,21 @@
 
 const calculateQuery = confirm("Should I calculate the square root?");
 if (calculateQuery) {
-  const numberInput = prompt(`Enter a number: `);
-  console.log(numberInput);
-  if (isNaN(numberInput) || numberInput < 0) {
-    alert("The square root of a negative number is not defined");
+  const numberInput = parseInt(prompt(`Enter a number: `));
+
+  if (numberInput < 0) {
+    document.querySelector("p").textContent =
+      "The square root of a negative number is not defined.";
+
+  } else if (!numberInput.isNumber) {
+    document.querySelector("p").textContent = "Please enter a valid number.";
+
+  } else {
+    let number = parseInt(numberInput);
+    let squareRoot = Math.sqrt(number);
+    document.querySelector("p").textContent =
+      "The square root of " + number + " is " + squareRoot + ".";
   }
-  let number = parseInt(numberInput);
-  console.log(number);
-  let squareRoot = Math.sqrt(number);
-  console.log(squareRoot);
-  document.querySelector("p").textContent =
-    "The square root of " + number + " is " + squareRoot + ".";
 } else {
   document.querySelector("p").textContent =
     "The square root is not calculated.";
