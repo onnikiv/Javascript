b92330eb1f324147e648972bc1e3ef614f2c3a98
onnik <onni.kivinen@gmail.com>
2024-04-08 21:44:23 +0300

    äyäy

---
+++ Module 1/Assignment 9/assignment9.js
@@ -0,0 +1,30 @@
+const primeNumber = parseInt(prompt("Give me a Number"));
+let primeNumberCheck
+// primeNumber % primeNumber == 0 && primeNumber % 1 == 0
+if (primeNumber % primeNumber == 0 && primeNumber % 1 == 0){
+  primeNumberCheck = true; 
+
+} else{
+  primeNumberCheck = false;
+}
+document.querySelector("p").textContent += primeNumberCheck ? "is yes" : "is no HOM";
+
+```
+if luku_string.isdigit():   #Tarkistetaan syöttö, jos luku niin tehdään merkkijonosta(str) luku(int).
+    luku = int(luku_string)
+
+    if luku < 2:    #Jos alle 2 ei voi olla alkuluku
+        print(f"{luku} ei ole alkuluku.")
+    else:
+        alkuluku = True  # Aluksi alkuluku on totta.
+        for i in range(2, int(luku ** 0.5) + 1):
+            if luku % i == 0:
+                alkuluku = False
+                break
+    if alkuluku:
+        print(f"{luku} on alkuluku.")
+    else:
+        print(f"{luku} ei ole alkuluku.")
+else:
+    print("Virheellinen syöte. Syötä kokonaisluku.")
+```
