1e144da4bfe4d063669c0ad144989da9d542a5b2
onnik <onni.kivinen@gmail.com>
2024-04-09 17:43:04 +0300

    finish

--- Module 1/Assignment 9/assignment9.js
+++ Module 1/Assignment 9/assignment9.js
@@ -1,26 +1,15 @@
 "use strict";
-let primeNumberCheck = parseInt(prompt("Give me a Number"));
-document.getElementById("p").innerHTML = testNumber
-for (let i =2; i < testNumber; i++){
-    if (testNumber % i == 0);
-        testNumber == true;
-        break
+const primeNumberInput = parseInt(prompt("Give me a Number"));
+
+let isPrimeNumber = primeNumberInput > 1; 
+for (let i = 2; i < primeNumberInput; i++) {
+    if (primeNumberInput % i === 0) {
+        isPrimeNumber = false;
+        break;
     }
-        testNumber == false
+}
 
+console.log(isPrimeNumber);
 
-document.querySelector("p").textContent += primeNumberCheck ? "YES" : "NO";
-
-```
-'use strict';
-        let testInt = prompt('Anna numero')
-        document.getElementById('result').innerHTML = testInt   
-        for(let i=2; i<testInt; i++){
-            if(testInt%i==0){
-                testInt = true
-                break
-            }
-            testInt = false
-        }
-        document.getElementById('result').innerHTML += testInt ? ' is not a prime number' : ' is a prime number'
-```
+const message = isPrimeNumber ? "Prime number: YES" : "Prime number: NO";
+document.querySelector("p").textContent += message;
