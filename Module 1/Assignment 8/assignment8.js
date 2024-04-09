deea515c7eacc6b81e893b92ca9f76f7f73fc6ec
onnik <onni.kivinen@gmail.com>
2024-04-09 17:50:58 +0300

    finish

--- Module 1/Assignment 8/assignment8.js
+++ Module 1/Assignment 8/assignment8.js
@@ -3,6 +3,8 @@ const endYear = prompt("Give me an Ending Year:");
 
 const unOrdered = document.querySelector("ul");
 
+// i = alkuvuosi; ehtona (kun) ollaan alle lopetusvuoden, ja sitten lisätään yksi (i),
+// listItem = luo li, sitten lisää i:llä saatu vuosi
 for (let i = startYear; i < endYear; i++){
     const listItem = document.createElement("li");
     listItem.innerText = i;
