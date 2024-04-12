a65bd6b8bd4a7f55acf60b771d601255c5167d45
onnik <onni.kivinen@gmail.com>
2024-04-12 11:12:10 +0300

    twiik

--- Tunti jutut/BOM-DOM.js
+++ Tunti jutut/BOM-DOM.js
@@ -1,57 +1,71 @@
 function locationSuccess(location) {
-  console.log("Käyttäjä paikannettu", location);
+    console.log('Käyttäjä paikannettu', location);
 }
 
 function locationError(error) {
-  console.log("paikannus fail", error);
+    console.log('paikannus fail', error);
 }
 
 const locationOptions = {
-  timeout: 5000,
+    timeout: 5000,
 };
 
 // käytetään tapahtumankäsittelijänä, kun p-näppäintä painetaan
 function locateUser(event) {
-  console.log("Näppäintapahtuma", event);
-  if (event.key === "p") {
-    navigator.geolocation.getCurrentPosition(
-      locationSuccess,
-      locationError,
-      locationOptions
-    );
-  }
+    console.log('Näppäintapahtuma', event);
+    if (event.key === 'p') {
+        navigator.geolocation.getCurrentPosition(
+            locationSuccess,
+            locationError,
+            locationOptions
+        );
+    }
 }
 
-console.log("Moro!");
+console.log('Moro!');
 
 // DOM-kikkailua
 
-const section2 = document.querySelectorAll("section")[1];
-const pElement = section2.querySelector("p");
-pElement.textContent = "muokattu";
+const section2 = document.querySelectorAll('section')[1];
+const pElement = section2.querySelector('p');
+pElement.textContent = 'muokattu';
 
-const newP = document.createElement("p");
-newP.textContent = "uusi kappale";
+const newP = document.createElement('p');
+newP.textContent = 'uusi kappale';
 section2.append(newP);
 // newP.style = 'color: blue';
-newP.classList.add("blue");
+newP.classList.add('blue');
 
-const thirdSection = document.querySelector("#third-section");
+const thirdSection = document.querySelector('#third-section');
 thirdSection.innerHTML = `
-      <h2>Kolmannen osion otsikko</h2>
-      <p>
-          Tässä taas tekstiä.
-      </p>`;
+            <h2>Kolmannen osion otsikko</h2>
+            <p>
+                    Tässä taas tekstiä.
+            </p>`;
 
 // Tapahtuman käsittely
 
-const buttonElement = document.querySelector("button");
-buttonElement.addEventListener("click", function () {
-  console.log("painettu napulaa");
-  // newP.classList.add('red');
-  // newP.classList.remove('blue');
-  newP.classList.toggle("red");
-  newP.classList.toggle("blue");
+const buttonElement = document.querySelector('button');
+buttonElement.addEventListener('click', function () {
+    console.log('painettu napulaa');
+    // newP.classList.add('red');
+    // newP.classList.remove('blue');
+    newP.classList.toggle('red');
+    newP.classList.toggle('blue');
 });
 
-document.addEventListener("keypress", locateUser);
+// näppäimistö
+document.addEventListener('keypress', locateUser);
+
+// hiiri
+`
+document.addEventListener('mousemove',(event) => {
+    console.log(event);
+});
+`
+
+document.addEventListener('contextmenu', function (event) {
+    console.log(event);
+    event.preventDefault();
+    alert('ei toimi');
+})
