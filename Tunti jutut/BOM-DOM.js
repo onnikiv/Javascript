12c9ee6d88c97f818313d00595470ce60c8ea767
onnik <onni.kivinen@gmail.com>
2024-04-12 10:38:35 +0300

    tweak

--- Tunti jutut/BOM-DOM.js
+++ Tunti jutut/BOM-DOM.js
@@ -1,23 +1,33 @@
+
 function locationSuccess (location) {
-    console.log("Käyttäjä paikannettu.", location);
-}
-
-function locationError (error) {
-    console.log("Käyttäjän paikannus epäonnistui!", error)
-}
-
-const locationOptions = {
-    timeout: 1000
-}
-navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
-
-console.log("moro")
-
-// DOM-kikkailu
-
-const section2 = document.querySelectorAll('section')[1];
-const pElement = section2.querySelector('p');
-pElement.textContent = 'muokattu';
-
-const newP = document.createElement('p');
-section2.append(newP);
+    console.log('Käyttäjä paikannettu', location);
+  }
+  
+  function locationError (error) {
+    console.log('paikannus fail', error);
+  }
+  
+  const locationOptions = {
+    timeout: 3000
+  };
+  
+  navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+  console.log('Moro!');
+  
+  // DOM-kikkailua
+  
+  const section2 = document.querySelectorAll('section')[1];
+  const pElement = section2.querySelector('p');
+  pElement.textContent = 'muokattu';
+  
+  const newP = document.createElement('p');
+  newP.textContent = 'uusi kappale';
+  section2.append(newP);
+  newP.style = 'color: blue';
+  
+  const thirdSection = document.querySelector('#third-section');
+  thirdSection.innerHTML = `
+      <h2>Kolmannen osion otsikko</h2>
+      <p>
+          Tässä taas tekstiä.
+      </p>`;
