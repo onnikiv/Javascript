c65d745de28ba79669287272c7bcea57d5a6de80
onnik <onni.kivinen@gmail.com>
2024-04-12 11:06:40 +0300

    adj

--- Tunti jutut/BOM-DOM.js
+++ Tunti jutut/BOM-DOM.js
@@ -1,36 +1,43 @@
-
-function locationSuccess (location) {
-    console.log('Käyttäjä paikannettu', location);
-  }
-  
-  function locationError (error) {
-    console.log('paikannus fail', error);
-  }
-  
-  const locationOptions = {
-    timeout: 3000
-  };
-  
-  function locateUser() {
-    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+function locationSuccess(location) {
+  console.log("Käyttäjä paikannettu", location);
 }
-  
-  console.log('Moro!');
-  
-  // DOM-kikkailua
-  
-  const section2 = document.querySelectorAll('section')[1];
-  const pElement = section2.querySelector('p');
-  pElement.textContent = 'muokattu';
-  
-  const newP = document.createElement('p');
-  newP.textContent = 'uusi kappale';
-  section2.append(newP);
-  // newP.style = 'color: blue';
-  newP.classList.add('blue');
 
-  const thirdSection = document.querySelector('#third-section');
-  thirdSection.innerHTML = `
+function locationError(error) {
+  console.log("paikannus fail", error);
+}
+
+const locationOptions = {
+  timeout: 5000,
+};
+
+// käytetään tapahtumankäsittelijänä, kun p-näppäintä painetaan
+function locateUser(event) {
+  console.log("Näppäintapahtuma", event);
+  if (event.key === "p") {
+    navigator.geolocation.getCurrentPosition(
+      locationSuccess,
+      locationError,
+      locationOptions
+    );
+  }
+}
+
+console.log("Moro!");
+
+// DOM-kikkailua
+
+const section2 = document.querySelectorAll("section")[1];
+const pElement = section2.querySelector("p");
+pElement.textContent = "muokattu";
+
+const newP = document.createElement("p");
+newP.textContent = "uusi kappale";
+section2.append(newP);
+// newP.style = 'color: blue';
+newP.classList.add("blue");
+
+const thirdSection = document.querySelector("#third-section");
+thirdSection.innerHTML = `
       <h2>Kolmannen osion otsikko</h2>
       <p>
           Tässä taas tekstiä.
@@ -38,13 +45,13 @@ function locationSuccess (location) {
 
 // Tapahtuman käsittely
 
-const buttonElement = document.querySelector('button');
-buttonElement.addEventListener('click', function() {
-    console.log('painettu napulaa');
-    // newP.classList.add('red');
-    // newP.classList.remove('blue');
-    newP.classList.toggle('red');
-    newP.classList.toggle('blue');
+const buttonElement = document.querySelector("button");
+buttonElement.addEventListener("click", function () {
+  console.log("painettu napulaa");
+  // newP.classList.add('red');
+  // newP.classList.remove('blue');
+  newP.classList.toggle("red");
+  newP.classList.toggle("blue");
 });
 
-document.addEventListener['keypress',locateUser]
+document.addEventListener("keypress", locateUser);
