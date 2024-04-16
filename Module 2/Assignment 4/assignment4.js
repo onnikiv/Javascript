035bee96fbd7260367406dbae6bb5411705f4799
onnik <onni.kivinen@gmail.com>
2024-04-16 10:50:50 +0300

    finish

---
+++ Module 2/Assignment 4/assignment4.js
@@ -0,0 +1,14 @@
+let kysytytNumerot = [];
+const nolla = 0;
+
+while (!kysytytNumerot.includes(nolla)) {
+    const luku = parseInt(prompt("Anna luku:"));
+    kysytytNumerot.push(luku);
+}
+
+kysytytNumerot.sort(function (a, b) {
+    return b - a;
+});
+console.log(kysytytNumerot);
+
+document.querySelector("p").textContent = kysytytNumerot;
