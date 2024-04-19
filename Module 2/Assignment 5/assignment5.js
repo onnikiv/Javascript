bf1fa60d780f55a2e9e0b13a04588630a0716ffd
onnik <onni.kivinen@gmail.com>
2024-04-19 13:36:31 +0300

    finish

--- Module 2/Assignment 5/assignment5.js
+++ Module 2/Assignment 5/assignment5.js
@@ -1,7 +1,17 @@
 let numbers = [];
 
-numberInput = parseInt(prompt("Give me a number:"));
-console.log(numberInput)
+while (true) {
+    const numberInput = parseInt(prompt('Give me a number:'));
+    console.log(numberInput);
 
-numbers.push(numberInput);
+    if (numbers.includes(numberInput)) {
+        console.log(`${numberInput} on jo listassa`);
+        break;
+    }
 
+    numbers.push(numberInput);
+}
+
+numbers.sort();
+document.querySelector('p').textContent = numbers;
+console.log(numbers);
