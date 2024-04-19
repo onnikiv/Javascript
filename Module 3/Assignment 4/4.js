eb98dcecc7ab34b08d96ec76175173712a038fc8
onnik <onni.kivinen@gmail.com>
2024-04-19 11:23:18 +0300

    finish

--- Module 3/Assignment 4/4.js
+++ Module 3/Assignment 4/4.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const students = [
   {
     name: 'John',
@@ -13,3 +14,13 @@ const students = [
     id: '5423679',
   },
 ];
+
+const idElement = document.getElementById('target');
+
+for (let i = 0; i < students.length; i++) {
+  const student = students[i];
+  const option = document.createElement('option');
+  option.value = student.id;
+  option.textContent = student.name;
+  idElement.appendChild(option);
+}
