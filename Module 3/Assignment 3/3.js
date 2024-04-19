bd63c47e6fe414f3c1d99f176c6155050c82688c
onnik <onni.kivinen@gmail.com>
2024-04-19 09:29:25 +0300

    tweaks

--- Module 3/Assignment 3/3.js
+++ Module 3/Assignment 3/3.js
@@ -1,2 +1,8 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+for (let i = 0; i < names.length; i++) { // i = 0, i alle names pituuden, lisätään i
+    const listItem = document.createElement('li');  // listItem luonti
+    listItem.textContent = names[i];  // listItemin teksti = nameslista item indeksillä
+    document.body.appendChild(listItem); // 
+}
